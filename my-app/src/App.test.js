4b1389decd20fd64a92c47ce060ead9d24d3cc0d
21grahams <graham.kirsh@gmail.com>
2021-02-26 19:10:26 -0700

    Comment out App.test "hello world"

--- my-app/src/App.test.js
+++ my-app/src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders hello world', () => {
-  render(<App />);
-  const helloWorldElement = screen.getByText('Hello World!');
-  expect(helloWorldElement).toBeInTheDocument();
-});
+// test('renders hello world', () => {
+//   render(<App />);
+//   const helloWorldElement = screen.getByText('Hello World!');
+//   expect(helloWorldElement).toBeInTheDocument();
+// });
