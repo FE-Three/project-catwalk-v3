5cbf2323d8b584d63eb10b225200e070a287aa1d
21grahams <graham.kirsh@gmail.com>
2021-03-03 21:19:09 -0700

    Fixed some CSS, added functionality to the add links, reported links and the answer dispalys / amounts

--- my-app/src/questionsAndAnswers/Answer.jsx
+++ my-app/src/questionsAndAnswers/Answer.jsx
@@ -2,30 +2,62 @@ import React from 'react';
 import Moment from 'react-moment';
 /* eslint-disable */
 
-const Answer = (props) => {
+class Answer extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      count: this.props.helpfulness,
+      helpfulClicked: false,
+      reportClick: false
+    };
 
-  //console.log('ANSWER COMPONENT: ', props)
+    this.helpfulnessToggle = this.helpfulnessToggle.bind(this);
+    this.reportToggle = this.reportToggle.bind(this);
+  }
 
-  return (
-    <div>
+  helpfulnessToggle() {
+    if (this.state.helpfulClicked === false) {
+      this.setState({
+        helpfulClicked: !this.state.helpfulClicked,
+        count: this.state.count + 1
+      });
+    } else {
+      this.setState({
+        helpfulClicked: !this.state.helpfulClicked,
+        count: this.state.count - 1
+      })
+    }
+  }
+
+  reportToggle() {
+    this.setState({
+      reportClick: !this.state.reportClick
+    })
+    alert('Thank you. Your answer has been recorded 🗣')
+  }
+
+  render() {
+    console.log('answers: ', this.props.answer);
+    return (
+      <div>
       <p className='answers'>
-        A:
-        <span>&nbsp;</span>
-        {props.answer}
+        A: &nbsp; {this.props.answer}
       </p>
-      <p className='userInfo'>
-        by
-        <span>&nbsp;</span>
-        {props.username + ','}
-        <span>&nbsp;</span>
-        <Moment format='MMMM D, YYYY' date={props.date} />
-        <span> &nbsp; &nbsp; &nbsp; &nbsp; |</span>
-        <span> &nbsp; &nbsp; &nbsp; &nbsp; Helpful? Yes(#)</span>
-        <span> &nbsp; &nbsp; &nbsp; &nbsp; |</span>
-        <span> &nbsp; &nbsp; &nbsp; &nbsp; Report</span>
-      </p>
+        <div id='aContainer'>
+          <p className='userInfo'>
+            by {this.props.username + ','} &nbsp;
+            <Moment className='date' format='MMMM D, YYYY' date={this.props.date} /> &nbsp;
+            <span className='dividerOne'> &nbsp; | &nbsp; </span>
+            <span className='answerHelpful' onClick={this.helpfulnessToggle}>Helpful? <span className='yes'>Yes</span>({this.state.count}) &nbsp; </span>
+            <span className='dividerTwo'> &nbsp; | &nbsp; </span>
+            {!this.state.reportClick ?
+            <span className='report'> &nbsp; <span onClick={this.reportToggle} className='reportText'>Report</span> &nbsp; </span>
+            : 'Reported'}
+          </p>
+        </div>
     </div>
-  );
+    )
+  };
 }
 
 export default Answer;
--- my-app/src/questionsAndAnswers/Question.jsx
+++ my-app/src/questionsAndAnswers/Question.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import axios from 'axios';
+// import axios from 'axios';
 import Answer from './Answer';
 /* eslint-disable */
 
@@ -9,13 +9,17 @@ class Question extends React.Component {
     this.state = {
       count: this.props.helpful,
       helpfulClicked: false,
-      clickedTestTwo: false
+      clickedTestTwo: false,
+      loadAllClick: false
     }
+    // console.log('answerOne: ', answerOne);
     this.helpfulToggle = this.helpfulToggle.bind(this);
     this.addAnswer = this.addAnswer.bind(this);
+    this.loadTwo = this.loadTwo.bind(this);
+    // this.loadAll = this.loadAll.bind(this);
   }
 
-  helpfulToggle(num) {
+  helpfulToggle() {
     if (this.state.helpfulClicked === false) {
       this.setState({
         helpfulClicked: !this.state.helpfulClicked,
@@ -44,22 +48,53 @@ class Question extends React.Component {
     })
   }
 
-
-  render() {
-    // console.log('NUMBER: ', this.state.count);
+  loadTwo() {
     const answerKeys = Object.values(this.props.answer);
-    const nextTest = this.state.clickedTestTwo ? 'yellow' : null;
-    return (
+    const sorted = answerKeys.sort((a, b) => {
+      return b.helpfulness - a.helpfulness
+    })
+    return sorted.slice(0, 2).map((answer, i) => (
+      <Answer answer={answer.body} username={answer.answerer_name} date={answer.date} helpfulness={answer.helpfulness} key={i} />
+      ))
+    }
 
+    // loadAll() {
+    //   if (this.state.helpfulClicked === false) {
+    //     this.setState({
+    //       loadAllClick: !this.state.loadAllClick,
+    //     });
+    //   const answerKeys = Object.values(this.props.answer);
+    //   const sorted = answerKeys.sort((a, b) => {
+    //     return b.helpfulness - a.helpfulness
+    //   })
+    //   return sorted.map((answer, i) => (
+    //     <Answer answer={answer.body} username={answer.answerer_name} date={answer.date} helpfulness={answer.helpfulness} key={i} />
+    //     ))
+    // }
+
+
+    render() {
+      const answerKeys = Object.values(this.props.answer);
+      const nextTest = this.state.clickedTestTwo ? 'yellow' : null;
+      // const loadingAll = this.state.loadAllClick ?
+      const sorted = answerKeys.sort((a, b) => {
+        return b.helpfulness - a.helpfulness
+    })
+    return (
       <div className="questions">
-        <div className='qContainer'>
+        <div id='qContainer'>
           <h4> Q: {this.props.question}</h4>
-          <div className='helpfulLink' onClick={this.helpfulToggle}>Helpful? <span className='yes'>Yes</span>({this.state.count})</div>
-          <div className='addAnswerLink' onClick={this.addAnswer}> Add Answer{nextTest}</div>
+          <div className='helpfulLink' onClick={this.helpfulToggle}>Helpful&nbsp;<span className='yes'>Yes</span>({this.state.count})&nbsp;&nbsp;</div>
+          <span className='divider'> | &nbsp;&nbsp; </span>
+          <div className='addAnswerLink' onClick={this.addAnswer}>Add Answer{nextTest}</div>
         </div>
-        {answerKeys.map((answer, i) => (
-          <Answer answer={answer.body} username={answer.answerer_name} date={answer.date} key={i} />
-          ))}
+        {answerKeys.length <= 2
+        ?
+        sorted.map((answer, i) => (
+          <Answer answer={answer.body} username={answer.answerer_name} date={answer.date} helpfulness={answer.helpfulness} key={i} />
+          ))
+          :
+          this.loadTwo()}
       </div>
     );
   }
