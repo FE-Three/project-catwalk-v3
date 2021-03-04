7c7136a4a0384a6e090acbf215594262a319c424
jbframe <framejb@gmail.com>
2021-03-04 16:59:16 -0700

    Init style container

--- my-app/src/ProductOverview/ProductStyle.jsx
+++ my-app/src/ProductOverview/ProductStyle.jsx
@@ -16,14 +16,26 @@ class ProductStyle extends React.Component {
   render() {
     return (
       <div className="style">
-        <div>Product style</div>
-        <div>
+        <div id="styleContainer">
+          <div className="price">
         {this.props.AppState.productStyles.results
           ?
-          `Price ${this.props.AppState.productStyles.results[0].original_price}`
+          `$${this.props.AppState.productStyles.results[0].original_price}`
           :
           ''
         }
+          </div>
+          <div className="styleH">STYLE > SELECTED STYLE</div>
+          <div className="TH1"></div>
+          <div className="TH2"></div>
+          <div className="TH3"></div>
+          <div className="TH4"></div>
+          <div className="TH5"></div>
+          <div className="TH6"></div>
+          <div className="TH7"></div>
+          <div className="TH8"></div>
+          <div className="WS"></div>
+
         </div>
       </div>
     )
