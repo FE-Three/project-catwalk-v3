15dc26acfe232c0ba9ff24bafcfeb61fc4306596
jbframe <framejb@gmail.com>
2021-03-05 20:14:28 -0700

    init style thumb nail render

--- my-app/src/ProductOverview/ProductStyle.jsx
+++ my-app/src/ProductOverview/ProductStyle.jsx
@@ -6,61 +6,66 @@ class ProductStyle extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-
     }
+    this.onStyleClick = this.onStyleClick.bind(this);
   }
 
   componentDidMount() {
+
+
+  }
+
+
+  onStyleClick (event) {
+    event.persist()
+    console.log(event);
   }
 
   render() {
-    //console.log(this.props.AppState.productStyles.results[0].photos[0].thumbnail_url)
-   console.log(this.props.AppState.productStyles.results)
+    let styles = () => {
+      if (this.props.AppState.productStyles.results) {
+        let styleOptions = this.props.AppState.productStyles.results;
+        return (
+          <div id="styleContainer">
+            <div className="price">
+              {`$${styleOptions[0].original_price}`}
+            </div>
+            <div className="styleH">STYLE > {styleOptions[0].name}</div>
+            <div className="TN1">
+              <img className="TN" onClick={this.onStyleClick} src={styleOptions[0].photos[0].thumbnail_url} />
+            </div>
+            <div className="TN2">
+              <img className="TN" onClick={this.onStyleClick} src={styleOptions[1].photos[0].thumbnail_url} />
+            </div>
+            <div className="TN3">
+              <div className="TN"></div>
+            </div>
+            <div className="TN4">
+              <div className="TN"></div>
+            </div>
+            <div className="TN5">
+              <div className="TN"></div>
+            </div>
+            <div className="TN6">
+              <div className="TN"></div>
+            </div>
+            <div className="TN7">
+              <div className="TN"></div>
+            </div>
+            <div className="TN8">
+              <div className="TN"></div>
+            </div>
+            <div className="WS"></div>
+          </div>
+        )
+      }
+    }
     return (
       <div className="style">
-        <div id="styleContainer">
-          <div className="price">
-        {this.props.AppState.productStyles.results
-          ?
-          `$${this.props.AppState.productStyles.results[0].original_price}`
-
-          :
-          ''
-        }
-          </div>
-          <div className="styleH">STYLE > SELECTED STYLE</div>
-          <div className="TN1">
-            <div className="TN"></div>
-          </div>
-          <div className="TN2">
-            <div className="TN"></div>
-          </div>
-          <div className="TN3">
-            <div className="TN"></div>
-          </div>
-          <div className="TN4">
-            <div className="TN"></div>
-          </div>
-          <div className="TN5">
-            <div className="TN"></div>
-          </div>
-          <div className="TN6">
-            <div className="TN"></div>
-          </div>
-          <div className="TN7">
-            <div className="TN"></div>
-          </div>
-          <div className="TN8">
-            <div className="TN"></div>
-          </div>
-          <div className="WS"></div>
-
-        </div>
+        {styles()}
       </div>
     )
   }
-
-
 }
 
 export default ProductStyle;
