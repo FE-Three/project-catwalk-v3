e96c7291ee45ea5e13d913e611aa3713d07ca922
21grahams <graham.kirsh@gmail.com>
2021-03-10 10:20:17 -0700

    Cleaned up CSS further including searchBar whitespace, added photo thumbnails, more answered questions functional

--- my-app/src/Ratings&Reviews/Recommend.jsx
+++ my-app/src/Ratings&Reviews/Recommend.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
+/* eslint-disable */
 
 const Recommend = ({ recommend }) => {
   if (recommend) {
     return <div>I recommend this product</div>;
+  } else {
+    return <React.Fragment></React.Fragment>
   }
 };
-
 export default Recommend;
--- my-app/src/questionsAndAnswers/Display.jsx
+++ my-app/src/questionsAndAnswers/Display.jsx
@@ -3,19 +3,32 @@ import Question from './Question';
 /* eslint-disable */
 
 const Display = (props) => {
+
   return (
     <div>
       {props.display
-        ? props.display.slice(0, 4).map((result, i) => (
-            <Question
-              prodID={props.prodID}
-              product={props.product}
-              question={result.question_body}
-              answer={result.answers}
-              helpful={result.question_helpfulness}
-              key={i}
-            />
-          ))
+        ? (props.clicked === false ?
+          props.display.slice(0, 4).map((result, i) => (
+              <Question
+                prodID={props.prodID}
+                product={props.product}
+                question={result.question_body}
+                answer={result.answers}
+                helpful={result.question_helpfulness}
+                key={i}
+              />
+            ))
+         :
+        props.display.map((result, i) => (
+          <Question
+            prodID={props.prodID}
+            product={props.product}
+            question={result.question_body}
+            answer={result.answers}
+            helpful={result.question_helpfulness}
+            key={i}
+          />
+        )))
         : "Loading Questions..."}
       <br></br>
     </div>
