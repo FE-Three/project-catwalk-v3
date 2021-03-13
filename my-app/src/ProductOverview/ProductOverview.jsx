a0a2fcde9a2cab14d6935cf8f0a7aae608ddcf70
jbframe <framejb@gmail.com>
2021-03-13 09:17:00 -0700

    removed social icons

--- my-app/src/ProductOverview/ProductOverview.jsx
+++ my-app/src/ProductOverview/ProductOverview.jsx
@@ -36,15 +36,9 @@ class ProductOverview extends React.Component {
         <ProductCart AppState={this.props.AppState} ProductState={this.state} key={this.state.selectedStyle} />
         <div className="whiteSpace"></div>
         <div className="social" style={{'paddingBottom': '10px'}}>
-          {/* <i class="fab fa-twitter-square"></i>
-          <i class="fab fa-facebook-square"></i>
-          <i class="fab fa-facebook"></i>
-          <i class="fab fa-pinterest-square"></i>
-          <i class="fab fa-pinterest"></i> */}
-
-          <i className="fab fa-facebook-f" style={{'paddingRight': '30px', 'fontSize':'150%'}}></i>
+          {/* <i className="fab fa-facebook-f" style={{'paddingRight': '30px', 'fontSize':'150%'}}></i>
           <i className="fab fa-twitter" style={{'paddingRight': '30px', 'fontSize':'150%'}}></i>
-          <i className="fab fa-pinterest-p" style={{'fontSize':'150%'}}></i>
+          <i className="fab fa-pinterest-p" style={{'fontSize':'150%'}}></i> */}
         </div>
         <ProductDescription AppState={this.props.AppState} ProductState={this.state} />
       </div>
