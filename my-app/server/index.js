2f68df4a25f6faafba61ccfef01cc8533630ae2b
nohren <orendnelson@gmail.com>
2021-03-19 15:50:20 -0700

    added a development url to the server index.js for easy development.  Added a place for us to add our respective get requests.

--- my-app/server/index.js
+++ my-app/server/index.js
@@ -7,15 +7,22 @@ const axios = require('axios');
 const cors = require('cors');
 const config = require('../config/config.js')
 
-app.use(express.static(path.join(__dirname, '../build')));
+/**********************change this url for development or production (aws) server**********************************/
+const url = 'http://localhost:8080';
+// 'https://app-hrsei-api.herokuapp.com/api/fec2/hr-bld'
 
-// app.use(express.static(path.join(__dirname, '../public')));
+/************************************************************************************* */
+//they have both a build and a public folder.  Can only change things with public.
+// all build stuff is commented out 
+// app.use(express.static(path.join(__dirname, '../build')));
+
+app.use(express.static(path.join(__dirname, '../public')));
 
 app.use(cors());
 app.use(express.json());
 
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "http://localhost:8080"); // update to match the domain you will make the request from
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "http://localhost:3000"); // update to match the domain you will make the request from
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
@@ -24,48 +31,62 @@ app.listen(port, () => {
   console.log(`Server listening at localhost:${port}!`);
 });
 
-app.get('/item/*', (req, res) =>{
-  res.sendFile(path.join(path.join(__dirname, '../build/index.html')));
-});
-app.get('/:id(\\d+)/', (req, res) =>{
-  res.sendFile(path.join(path.join(__dirname, '../build/index.html')));
-});
-
 // app.get('/item/*', (req, res) =>{
-//   res.sendFile(path.join(path.join(__dirname, '../public/index.html')));
+//   res.sendFile(path.join(path.join(__dirname, '../build/index.html')));
 // });
 // app.get('/:id(\\d+)/', (req, res) =>{
-//   res.sendFile(path.join(path.join(__dirname, '../public/index.html')));
+//   res.sendFile(path.join(path.join(__dirname, '../build/index.html')));
 // });
 
-
-app.get('*', (req, res) => {
-  axios({method: 'get',
-  headers: {'Authorization': config.config},
-  url: `https://app-hrsei-api.herokuapp.com/api/fec2/hr-bld${req.url}`,
-})
-
-.then(response => {
-  // console.log(response);
-  res.status(200).send(response.data);
-})
-.catch(err => {
-  //console.log(err);
-  res.status(err.response.status).send(err.response.data);
+app.get('/item/*', (req, res) => {
+  res.sendFile(path.join(path.join(__dirname, '../public/index.html')));
 });
+app.get('/:id(\\d+)/', (req, res) => {
+  res.sendFile(path.join(path.join(__dirname, '../public/index.html')));
+});
+
+//any get reqeust, append url that is different
+app.get('*', (req, res) => {
+  console.log(req.url)
+  if (req.url.indexOf('questions') !== -1) {
+    console.log('questions requests: ', req.url)
+    axios({
+      method: 'get',
+      headers: { 'Authorization': config.config },
+      url: `${url}${req.url}`,
+    })
+
+      .then(response => {
+        // console.log(response);
+        // console.log(response.data)
+        res.status(200).send(response.data);
+      })
+      .catch(err => {
+        //console.log(err);
+        res.status(err.response.status).send(err.response.data);
+      });
+  }
+  else if (req.url.indexOf('products') !== -1) {
+    //axios get requests to products server
+
+  } else if (req.url.indexOf('reviews') !== -1) {
+    //axios get requests to reviews server
+
+  }
+
 })
 
 
 //=======================================
-          // add question
+// add question
 //=======================================
 app.post('/qa/questions', (req, res) => {
   axios({
     method: 'post',
-    headers: {'Authorization': config.config},
-    url: `https://app-hrsei-api.herokuapp.com/api/fec2/hr-bld/qa/questions/`,
+    headers: { 'Authorization': config.config },
+    url: `${url}/qa/questions/`,
     data: {
-      body : req.body.body,
+      body: req.body.body,
       name: req.body.name,
       email: req.body.email,
       product_id: req.body.product_id
@@ -74,83 +95,83 @@ app.post('/qa/questions', (req, res) => {
     .then(result => {
       console.log('POSTED')
       res.sendStatus(201);
-  })
+    })
     .catch(err => console.log('ERROR POSTING: ', err))
 })
 
 
 
 //=======================================
-          // add answer
+// add answer
 //=======================================
 app.post('/qa/questions/answers', (req, res) => {
   axios({
     method: 'post',
-    headers: {'Authorization': config.config},
-    url: `https://app-hrsei-api.herokuapp.com/api/fec2/hr-bld/qa/questions/${req.body.question_id}/answers`,
+    headers: { 'Authorization': config.config },
+    url: `${url}/qa/questions/${req.body.question_id}/answers`,
     data: {
-      body : req.body.body,
+      body: req.body.body,
       name: req.body.name,
       email: req.body.email,
       photos: req.body.photos
     }
   })
-  .then(response => {
-    res.status(201).send('Post has been recorded')
-  })
-  .catch(err => console.log('error with post request: ', err))
+    .then(response => {
+      res.status(201).send('Post has been recorded')
+    })
+    .catch(err => console.log('error with post request: ', err))
 })
 
 
 //=======================================
-    // put request QUESTION HELPFUL
+// put request QUESTION HELPFUL
 //=======================================
 app.put('/qa/questions/:question_id/helpful', (req, res) => {
   axios({
     method: 'put',
-    headers: {'Authorization': config.config},
-    url: `https://app-hrsei-api.herokuapp.com/api/fec2/hr-bld/qa/questions/${req.params.question_id}/helpful`,
+    headers: { 'Authorization': config.config },
+    url: `${url}/qa/questions/${req.params.question_id}/helpful`,
   })
-  .then(response => {
-    res.status(201).send('Post has been recorded')
-  })
-  .catch(err => console.log('error with post request: ', err))
+    .then(response => {
+      res.status(201).send('Post has been recorded')
+    })
+    .catch(err => console.log('error with post request: ', err))
 })
 
 //=======================================
-      // put request ANSWER HELPFUL
+// put request ANSWER HELPFUL
 //=======================================
 app.put(`/qa/answers/:answer_id/helpful`, (req, res) => {
-  axios.put(`https://app-hrsei-api.herokuapp.com/api/fec2/hr-bld/qa/answers/${req.params.answer_id}/helpful`, {}, {
+  axios.put(`${url}/qa/answers/${req.params.answer_id}/helpful`, {}, {
     headers: {
       'Authorization': config.config
     },
   })
-  .then((response) => {
-    res.status(200);
-    res.send(response.data);
-  })
-  .catch((err) => {
-    res.status(404);
-    console.log('ERROR WITH ANSWER IS HELPFUL: ', err)
-  })
+    .then((response) => {
+      res.status(200);
+      res.send(response.data);
+    })
+    .catch((err) => {
+      res.status(404);
+      console.log('ERROR WITH ANSWER IS HELPFUL: ', err)
+    })
 })
 
 //=======================================
-     // put request REPORT ANSWER
+// put request REPORT ANSWER
 //=======================================
 app.put(`/qa/answers/:answer_id/report`, (req, res) => {
-  axios.put(`https://app-hrsei-api.herokuapp.com/api/fec2/hr-bld/qa/answers/${req.params.answer_id}/report`, {}, {
+  axios.put(`${url}/qa/answers/${req.params.answer_id}/report`, {}, {
     headers: {
       'Authorization': config.config
     },
   })
-  .then((response) => {
-    res.status(200);
-    res.send(response.data);
-  })
-  .catch((err) => {
-    res.status(404);
-    console.log('ERROR WITH ANSWER IS REPORTED: ', err)
-  })
+    .then((response) => {
+      res.status(200);
+      res.send(response.data);
+    })
+    .catch((err) => {
+      res.status(404);
+      console.log('ERROR WITH ANSWER IS REPORTED: ', err)
+    })
 })
